28fc1cf678e17e44f87f053bd2741c529db39397
zaripov0333-cmd <zaripov0333@gmail.com>
2026-02-25 00:17:04 +0500

    Refactor sendData function and add validation steps

--- submit.js
+++ submit.js
@@ -1,49 +1,49 @@
 const scriptURL = "https://script.google.com/macros/s/AKfycbzqFiYXPcrIzIGIQqF_0jaUbNHd9JyzYIAoND740mWtm6HmpRfmDZ-hs1aCZfSmAEJOhw/exec";
 
+function next(step){
+
+  if(step === 1){
+    if(!district.value) return alert("Manzilni tanlang");
+  }
+
+  if(step === 2){
+    if(!transport.value) return alert("Tanlang");
+  }
+
+  if(step === 3){
+    if(!salary.value) return alert("Tanlang");
+  }
+
+  document.getElementById("q"+step).classList.remove("active");
+  document.getElementById("q"+(step+1)).classList.add("active");
+}
+
 function sendData(){
 
-  const district = document.getElementById("district").value;
-  const transport = document.getElementById("transport").value;
-  const salary = document.getElementById("salary").value;
-
-  const name = document.getElementById("name").value;
-  const age = document.getElementById("age").value;
-  const phone = document.getElementById("phone").value;
-
-  const msg = document.getElementById("msg");
-  const extra = document.getElementById("extra");
-
-  if(transport === "HA" && salary === "HA"){
-      extra.classList.remove("hidden");
-
-      if(name && age && phone){
-
-        fetch(scriptURL, {
-          method: "POST",
-          body: JSON.stringify({
-            district: district,
-            transport: transport,
-            salary: salary,
-            name: name,
-            age: age,
-            phone: phone
-          }),
-          headers: {
-            "Content-Type": "application/json"
-          }
-        })
-        .then(res => {
-          msg.style.color = "green";
-          msg.innerText = "Arizangiz qabul qilindi!";
-        })
-        .catch(err => {
-          msg.style.color = "red";
-          msg.innerText = "Xatolik yuz berdi!";
-        });
-      }
-
-  } else {
-      msg.style.color = "red";
-      msg.innerText = "Faqat transporti bor va ish haqi to'g'ri keladiganlar qabul qilinadi!";
+  if(transport.value !== "HA" || salary.value !== "HA"){
+    msg.innerText = "Talabga mos emassiz.";
+    msg.style.color="red";
+    return;
   }
+
+  fetch(scriptURL,{
+    method:"POST",
+    body:JSON.stringify({
+      district:district.value,
+      transport:transport.value,
+      salary:salary.value,
+      name:name.value,
+      age:age.value,
+      phone:phone.value
+    }),
+    headers:{ "Content-Type":"application/json" }
+  })
+  .then(()=> {
+    msg.innerText="Ariza yuborildi!";
+    msg.style.color="green";
+  })
+  .catch(()=> {
+    msg.innerText="Xatolik yuz berdi";
+    msg.style.color="red";
+  });
 }
